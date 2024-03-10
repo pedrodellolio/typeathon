2a06fac3d227b151c8fe3caf6fe0e5fd9607f983
pedrodellolio <pedrohdellolio@gmail.com>
2024-03-10 15:37:43 -0300

    Improving typing test logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-
 const words = [
   "Amor",
   "Feliz",
@@ -49,6 +48,7 @@ function App() {
   const [currentTestWords, setCurrentTestWords] = useState<Word[]>([]);
 
   useEffect(() => {
+    // Get a set of words for current test
     const list: Word[] = words.slice(0, 30).map((word) => {
       const letters: Letter[] = word
         .toLowerCase()
@@ -64,11 +64,21 @@ function App() {
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const typedValue = e.target.value;
-    setInput(typedValue);
+    const segments = typedValue.split(" ");
+
+    // Prevent the deletion of previous correctly typed words
+    if (segments.length > currentWordIndex) setInput(typedValue);
+
+    setCurrentLetterIndex(
+      segments[currentWordIndex] ? segments[currentWordIndex].length : 0
+    );
 
     const currentWord = currentTestWords[currentWordIndex];
-    const typedWord = typedValue.toLowerCase().trim();
+    const typedWord = segments[currentWordIndex]
+      ? segments[currentWordIndex].toLowerCase().trim()
+      : "";
 
+    // Updates letter state based on typed input: 0 - error; 1 - correct; undefined - unreached letter
     const updatedLetters = currentWord.letters.map((letter, index) => {
       if (typedWord[index] === undefined) {
         return { ...letter, state: undefined };
@@ -78,29 +88,63 @@ function App() {
         return { ...letter, state: 0 };
       }
     });
-
     const updatedWord = { ...currentWord, letters: updatedLetters };
-
     setCurrentTestWords((prevState) =>
       prevState.map((word, index) =>
         index === currentWordIndex ? updatedWord : word
       )
     );
 
-    setCurrentLetterIndex(typedValue.length);
+    // Pressing space jumps to next word
+    const event = e.nativeEvent as any;
+    if (
+      typedValue.endsWith(" ") &&
+      event.inputType !== "deleteContentBackward"
+    ) {
+      //set all unreached words as error if player jumps to next word
+      setCurrentTestWords((prevState) =>
+        prevState.map((word, index) => {
+          if (index === currentWordIndex) {
+            return {
+              ...word,
+              letters: word.letters.map((letter) => ({
+                ...letter,
+                state: letter.state === undefined ? 0 : letter.state,
+              })),
+            };
+          }
+          return word;
+        })
+      );
 
-    if (typedValue.endsWith(" ")) {
-      setInput("");
+      //redefining indexes
       setCurrentLetterIndex(0);
       setCurrentWordIndex(currentWordIndex + 1);
     }
   };
 
-  console.log(currentTestWords[currentWordIndex]);
+  //handles return to previous incorrectly typed word
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Backspace") {
+      if (currentWordIndex > 0 && currentLetterIndex === 0) {
+        const previousWord = currentTestWords[currentWordIndex - 1];
+        const lastWrongLetter = previousWord.letters.find(
+          (l) => !l.state || l.state === 0
+        );
+        if (previousWord.letters.some((l) => l.state === 0)) {
+          setCurrentWordIndex(currentWordIndex - 1);
+          setCurrentLetterIndex(previousWord.letters.indexOf(lastWrongLetter!));
+        }
+      }
+    }
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center mt-56">
-      <div className="overflow-hidden flex flex-row flex-wrap w-[800px] h-[110px] gap-3 text-xl">
+    <div className="flex flex-col items-center justify-center mt-56 mx-auto max-w-[800px]">
+      <button className="self-start mb-6 border border-gray-300 px-2 py-1 rounded-md">
+        Restart
+      </button>
+      <div className="overflow-hidden flex flex-row flex-wrap w-full h-[110px] gap-3 text-xl">
         {currentTestWords.map((word, wIndex) => {
           const isCurrentWord = wIndex === currentWordIndex;
           return (
@@ -141,7 +185,9 @@ function App() {
         autoSave="off"
         type="text"
         value={input}
+        contentEditable={false}
         onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
         autoFocus
       />
     </div>
